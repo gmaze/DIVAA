14f2c151d15ff263c32bfc885384aa4619c9e602
Guillaume Maze <code@guillaumemaze.org>
2019-03-08 13:38:32 +0100

    Fix py36

--- bin/andro2json.sh
+++ bin/andro2json.sh
@@ -2,6 +2,7 @@
 #K.BALEM
 #2017
 ################
+source activate obidam36
 python andro2json.py $1 > temp
 sed -i 's/masked/0.0/g' temp
 cat temp | json_reformat > ../data/andro1000.json
--- bin/aviso2json.sh
+++ bin/aviso2json.sh
@@ -2,6 +2,7 @@
 #K.BALEM
 #2017
 ################
+source activate obidam36
 python aviso2json.py $1 > temp
 sed -i 's/masked/0.0/g' temp
 cat temp | json_reformat > ../data/aviso.json
